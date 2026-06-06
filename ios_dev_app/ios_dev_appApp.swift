69290eb9308f779c4e5e2f78fb47dd69ca3b24a9
diNeLlli <dinellisamarakkody@gmail.com>
2026-06-06 15:40:11 +0530

    Initial Commit

---
+++ ios_dev_app/ios_dev_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  ios_dev_appApp.swift
+//  ios_dev_app
+//
+//  Created by student2 on 2026-06-06.
+//
+
+import SwiftUI
+
+@main
+struct ios_dev_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
