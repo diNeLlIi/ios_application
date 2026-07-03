7486713b314a1aac0dd69ff5f5f181fa4c599388
diNeLlli <dinellisamarakkody@gmail.com>
2026-07-03 18:06:50 +0530

    data model

--- ios_dev_app/QuizRush/Models/TriviaModels.swift
+++ ios_dev_app/QuizRush/Models/TriviaModels.swift
@@ -5,3 +5,22 @@
 //  Created by student2 on 2026-06-30.
 //
 
+import Foundation
+
+struct TriviaResponse: Codable {
+    let results: [Question]
+}
+
+struct Question: Codable, Identifiable {
+    let id = UUID()
+    let category: String
+    let question: String
+    let correctAnswer: String
+    let incorrectAnswers: [String]
+
+    enum CodingKeys: String, CodingKey {
+        case category, question
+        case correctAnswer = "correct_answer"
+        case incorrectAnswers = "incorrect_answers"
+    }
+}
